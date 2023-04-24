ebc6959f2bab436f96a0b01be928e570dbb9aeca
Karlo Dela Rosa <jrosa@salespond.com>
2023-04-24 21:02:53 +0800

    updated init file

--- mysql/init.sql
+++ mysql/init.sql
@@ -29,4 +29,4 @@ CREATE TABLE note_label (
     primary key (noteId, labelId)
 );
 
-INSERT INTO user (`name`, `surname`) VALUES ('Karlo', 'Dela Rosa');
+INSERT INTO user (`name`, `surname`, `password`) VALUES ('Karlo', 'Dela Rosa', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
